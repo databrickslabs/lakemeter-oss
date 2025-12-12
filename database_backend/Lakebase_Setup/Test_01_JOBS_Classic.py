da6111770b701ef7d83783e0ae3fb66dfc1c04cf
Steven Tan <cheeyutcy@gmail.com>
2025-12-12 19:22:11 +0800

    Update test notebook: Hardcode Lakebase password for easier testing

--- database_backend/Lakebase_Setup/Test_01_JOBS_Classic.py
+++ database_backend/Lakebase_Setup/Test_01_JOBS_Classic.py
@@ -46,7 +46,7 @@ LAKEBASE_HOST = "lakebase-pricing.postgres.database.azure.com"
 LAKEBASE_PORT = 5432
 LAKEBASE_DB = "lakemeter_pricing"
 LAKEBASE_USER = "lakemeter_sync_role"
-LAKEBASE_PASSWORD = dbutils.secrets.get(scope="lakemeter", key="lakebase_password")
+LAKEBASE_PASSWORD = "***REMOVED_DATABASE_CREDENTIAL***"
 
 def get_connection():
     """Create and return a PostgreSQL connection"""
